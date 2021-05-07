cd30860ee4d89c1c357a77b25f5c26535cbf4c7c
rakib3004 <bsse1129@iit.du.ac.bd>
2021-05-07 08:58:18 +0600

    add pull.sh file

---
+++ pull.sh
@@ -0,0 +1,18 @@
+cd  codeforces-problem-solving ; git pull ; cd ../ ;
+cd  IntroductionToGitAndGithub ; git pull ; cd ../ ; 
+cd  CodingBookMarks   ; git pull ; cd ../ ;             
+cd  it-cert-automation-practice ; git pull ; cd ../ ; 
+cd  combinatorial-optimization ; git pull ; cd ../ ; 
+cd  numerical-analysis ; git pull ; cd ../ ; 
+cd  Coursera         ; git pull ; cd ../ ; 
+cd  OperatingSystem ; git pull ; cd ../ ; 
+cd  data-structure-algorithm ; git pull ; cd ../ ; 
+cd  ProgrammingContestMaterials ; git pull ; cd ../ ; 
+cd  example_repo  ; git pull ; cd ../ ; 
+cd  RiLanguageCenter ; git pull ; cd ../ ; 
+cd  hackkerrank-problem-solving  ; git pull ; cd ../ ;  
+cd  RoadToICPC ; git pull ; cd ../ ; 
+cd  HTML-Learning    ; git pull ; cd ../ ;       
+cd  virtual-judge-problem-solving ; git pull ; cd ../ ; 
+cd  InformationSecurity  ; git pull ; cd ../ ; 
+
